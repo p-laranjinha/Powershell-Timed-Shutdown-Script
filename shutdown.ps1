c8f101782eca742e9d933bae6293f95656874ed5
p-laranjinha <plcasimiro@ua.pt>
2023-11-01 01:16:30 +0000

    Cancel timer refactoring + formatting

--- shutdown.ps1
+++ shutdown.ps1
@@ -11,6 +11,18 @@ function ShowNotification($title, $text) {
     $balloon.ShowBalloonTip(3000)
 }
 
+function CancelTimer {
+    if (Get-ScheduledTask | Where-Object { $_.TaskName -like $taskname }) {
+        Unregister-ScheduledTask -TaskPath '\' -TaskName $taskname -Confirm:$false
+        if (!(Get-ScheduledTask | Where-Object { $_.TaskName -like $taskname })) {
+            ShowNotification $taskname "Timer has been cancelled."
+        }
+        else {
+            ShowNotification $taskname "ERROR: Timer could not be cancelled."
+        }
+    }
+}
+
 $form = New-Object System.Windows.Forms.Form
 $form.Text = 'Shutdown Timer'
 $form.Width = 370
@@ -19,7 +31,7 @@ $form.FormBorderStyle = 'FixedDialog';
 
 $label = New-Object System.Windows.Forms.Label
 $label.Text = "Minutes:"
-$label.Location  = New-Object System.Drawing.Point(10,12)
+$label.Location = New-Object System.Drawing.Point(10, 12)
 $label.AutoSize = $true
 $form.Controls.Add($label)
 
@@ -27,45 +39,36 @@ $numbox = New-Object System.Windows.Forms.NumericUpDown
 $numbox.Value = 60
 $numbox.Maximum = [int]::MaxValue;
 $numbox.Width = 70
-$numbox.Location = New-Object System.Drawing.Point(70,10)
+$numbox.Location = New-Object System.Drawing.Point(70, 10)
 $form.Controls.Add($numbox)
 
 $startbutton = New-Object System.Windows.Forms.Button
 $startbutton.Text = "Start Timer"
 $startbutton.Width = 90
-$startbutton.Location = New-Object System.Drawing.Point(150,10)
+$startbutton.Location = New-Object System.Drawing.Point(150, 10)
 $form.Controls.Add($startbutton)
 
 $stopbutton = New-Object System.Windows.Forms.Button
 $stopbutton.Text = "Stop Timer"
 $stopbutton.Width = 90
-$stopbutton.Location = New-Object System.Drawing.Point(250,10)
+$stopbutton.Location = New-Object System.Drawing.Point(250, 10)
 $form.Controls.Add($stopbutton)
 
 $taskname = "Timed Shutdown"
 
 $startbutton.Add_Click({
-    if (Get-ScheduledTask | Where-Object {$_.TaskName -like $taskname }) {
-        Unregister-ScheduledTask -TaskPath '\' -TaskName $taskname -Confirm:$false
-    }
+        CancelTimer
 
-    $time = (Get-Date).AddMinutes($numbox.Value)
-    $taskAction = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument 'Stop-Computer -Force'
-    $taskTrigger = New-ScheduledTaskTrigger -Once -At $time
-    Register-ScheduledTask -TaskName $taskname -Action $taskAction -Trigger $taskTrigger -Description "Computer will shutdown on $time"
+        $time = (Get-Date).AddMinutes($numbox.Value)
+        $taskAction = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument 'Stop-Computer -Force'
+        $taskTrigger = New-ScheduledTaskTrigger -Once -At $time
+        Register-ScheduledTask -TaskName $taskname -Action $taskAction -Trigger $taskTrigger -Description "Computer will shutdown on $time"
 
-    ShowNotification $taskname "Computer will shutdown on $time."
-})
+        ShowNotification $taskname "Computer will shutdown on $time."
+    })
 
 $stopbutton.Add_Click({
-    if (Get-ScheduledTask | Where-Object {$_.TaskName -like $taskname }) {
-        Unregister-ScheduledTask -TaskPath '\' -TaskName $taskname -Confirm:$false
-        if (!(Get-ScheduledTask | Where-Object {$_.TaskName -like $taskname })) {
-            ShowNotification $taskname "Timer has been cancelled."
-        } else {
-            ShowNotification $taskname "ERROR: Timer could not be cancelled."
-        }
-    }
-})
+        CancelTimer
+    })
 
 $form.ShowDialog()
